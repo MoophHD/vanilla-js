90740d79f19a0e6f36a384666dfdb3c1e9ec5d67
MoophHD <d_sizykh@mail.ru>
2020-10-29 19:05:48 +0300

    wrapped connectTasks in IIFE, minor style fixes

--- src/js/connectTasks.js
+++ src/js/connectTasks.js
@@ -1,43 +1,45 @@
-const tasks = {
-  dateDisplayFormatter,
-  textFormatter,
-};
+(() => {
+  const tasks = {
+    dateDisplayFormatter,
+    textFormatter,
+  };
 
-const taskNodes = document.querySelectorAll(".task");
+  const taskNodes = document.querySelectorAll(".task");
 
-[...taskNodes].forEach((task) => {
-  const taskName = task.id;
-  const liveMethods = task.querySelectorAll(".task-live .task__method");
-  const exampleMethods = task.querySelectorAll(".task-example .task__method");
+  [...taskNodes].forEach((task) => {
+    const taskName = task.id;
+    const liveMethods = task.querySelectorAll(".task-live .task__method");
+    const exampleMethods = task.querySelectorAll(".task-example .task__method");
 
-  [...liveMethods].forEach((liveMethod) => {
-    const methodName = liveMethod.dataset.methodName;
-    const inputs = [...liveMethod.querySelectorAll(".method__input")];
-    const output = liveMethod.querySelector(".task__output");
+    [...liveMethods].forEach((liveMethod) => {
+      const methodName = liveMethod.dataset.methodName;
+      const inputs = [...liveMethod.querySelectorAll(".method__input")];
+      const output = liveMethod.querySelector(".task__output");
 
-    inputs.forEach((input) => {
-      input.addEventListener("change", (e) => {
-        const inputs = [
-          ...e.target.parentElement.querySelectorAll(".method__input"),
-        ];
-        const arguments = inputs.map((input) =>
-          input.value ? input.value : input.getAttribute("placeholder")
-        );
+      inputs.forEach((input) => {
+        input.addEventListener("change", (e) => {
+          const inputs = [
+            ...e.target.parentElement.querySelectorAll(".method__input"),
+          ];
+          const arguments = inputs.map((input) =>
+            input.value ? input.value : input.getAttribute("placeholder")
+          );
 
-        output.innerText = tasks[taskName][methodName](...arguments);
+          output.innerText = tasks[taskName][methodName](...arguments);
+        });
       });
     });
+
+    [...exampleMethods].forEach((exampleMethod) => {
+      const methodName = exampleMethod.dataset.methodName;
+      const inputs = [...exampleMethod.querySelectorAll(".method__input")];
+      const output = exampleMethod.querySelector(".task__output");
+
+      const arguments = inputs.map((input) =>
+        input.value ? input.value : input.getAttribute("placeholder")
+      );
+
+      output.innerText = tasks[taskName][methodName](...arguments);
+    });
   });
-
-  [...exampleMethods].forEach((exampleMethod) => {
-    const methodName = exampleMethod.dataset.methodName;
-    const inputs = [...exampleMethod.querySelectorAll(".method__input")];
-    const output = exampleMethod.querySelector(".task__output");
-
-    const arguments = inputs.map((input) =>
-      input.value ? input.value : input.getAttribute("placeholder")
-    );
-
-    output.innerText = tasks[taskName][methodName](...arguments);
-  });
-});
+})();
